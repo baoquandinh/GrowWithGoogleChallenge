778db48279a05150dcdb312bcb660c50e4f65d8c
Baoquan Dinh <baoquanxdinh@gmail.com>
2018-07-16 20:00:09 -0700

    Practice on writing asynchronous and regular test suites

--- Phase 2/Testing Lesson/ud549-master/src/AddressBook.js
+++ Phase 2/Testing Lesson/ud549-master/src/AddressBook.js
@@ -1,11 +1,22 @@
 class AddressBook {
     constructor() {
         this.addressBook = [];
+        this.initialComplete = false;
+    }
+
+    // Fake API call for writing asynchronous test suites created for this lesson
+    getInitialContacts(cb) {
+        let self = this;
+        setTimeout(function() {
+            self.initialComplete = true;
+            if(cb) {
+                return cb();
+            }
+        }, 3);
     }
 
     addContact(contact) {
         this.addressBook.push(contact);
-        
         return contact;
     }
 
@@ -13,7 +24,7 @@ class AddressBook {
         delete this.addressBook[contact];
     }
 
-    getContact(contact) {
-        return this.addressBook[contact];
+    getContact(index) {
+        return this.addressBook[index];
     }
 }
